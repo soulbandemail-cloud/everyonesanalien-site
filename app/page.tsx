4e7f955346bd572d250d54e7c64b7a6d65e05868
Soul <soul.band.email@gmail.com>
2026-06-05 11:37:32 +0100

    Update merch text

--- app/page.tsx
+++ app/page.tsx
@@ -55,7 +55,7 @@ export default function Home() {
       <section className="mb-16">
         <h2 className="text-2xl mb-4">MERCH</h2>
 
-        <p>COMING SOON</p>
+        <p>FIRST DROP COMING SOON</p>
       </section>
 
       <footer className="border-t border-green-400 pt-6">
