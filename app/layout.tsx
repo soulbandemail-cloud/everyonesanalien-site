75624a5f15e7449350562cf55793ca81d5a3baa3
Soul <soul.band.email@gmail.com>
2026-06-05 14:31:35 +0100

    Add CRT-style font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Share_Tech_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const shareTechMono = Share_Tech_Mono({
+  weight: "400",
   subsets: ["latin"],
 });
 
@@ -23,11 +18,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
-    </html>
+    <html lang="en" className="h-full">
+  <body className={`${shareTechMono.className} min-h-full flex flex-col tracking-wider`}>
+    {children}
+  </body>
+</html>
   );
 }
