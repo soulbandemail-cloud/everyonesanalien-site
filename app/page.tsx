be150c38e5e2000283dc2945a5e128e98abb0230
Soul <soul.band.email@gmail.com>
2026-06-05 23:58:00 +0100

    “gap3“

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
 
     <main className="min-h-screen text-white p-8 md:p-12 max-w-6xl mx-auto">
 
- <div className="flex flex-wrap gap-12 mb-4 md:mb-4">
+ <div className="flex flex-wrap gap-18 mb-4 md:mb-4">
 
 <a
     href="https://open.spotify.com/artist/4aoqsXn1YULl9y1boDeTZA?si=mPVXh9BtR4KvRUSQoIyJYA"
