a62727da112e1565a6e56da0074bd5adbebac89d
Soul <soul.band.email@gmail.com>
2026-06-06 22:45:24 +0100

    “Waterloo2“

--- app/page.tsx
+++ app/page.tsx
@@ -83,7 +83,7 @@ export default function Home() {
 
   <span>S</span>
 
-  <span className="relative inline-flex items-center justify-center w-28 h-20 mx-1">
+  <span className="relative inline-flex items-center justify-center w-24 h-16 mx-1">
 
     {/* Heart */}
     <span
