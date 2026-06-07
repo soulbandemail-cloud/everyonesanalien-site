a5c4e9eb1e7a9c0214433e3cde2d36dd8ab688ba
Soul <soul.band.email@gmail.com>
2026-06-07 14:17:35 +0100

    “codexisabeast“

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,13 +10,16 @@ const shareTechMono = Share_Tech_Mono({
 export const metadata: Metadata = {
   title: "everyonesanalien.com",
   description: "Soul's official band website",
-  icons: {
-    icon: [
-      { url: "/favicon-32x32.png?v=14", type: "image/png", sizes: "32x32" },
-    ],
-    shortcut: "/favicon-32x32.png?v=14",
-  },
+ icons: {
+  icon: [
+    { url: "/favicon.svg?v=15", type: "image/svg+xml" },
+    { url: "/favicon-32x32.png?v=15", type: "image/png", sizes: "32x32" },
+  ],
+  apple: "/apple-touch-icon.png?v=15",
+}
 };
+
+
 
 export default function RootLayout({
   children,
